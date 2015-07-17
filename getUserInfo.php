85395466a07b0275f395e3ee05049fe447103e4d
Lu Sun <sunlufinal@hotmail.com>
2015-07-16 21:07:12 -0400

    Tested getting user info

---
+++ getUserInfo.php
@@ -0,0 +1,33 @@
+<?php
+if(isset($_GET['code'])) {
+	$code = $_GET['code'];
+	echo $code.'<br>';
+
+	$json = file_get_contents('https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxa0be4dbfe322ad09&secret=af5bfbe98b02a77a22b3da239aaa94df&code='.$code.'&grant_type=authorization_code');
+	$json_object = json_decode($json);
+	$access_token = $json_object->access_token;
+	$openid = $json_object->openid;
+	echo 'access_token: '.$access_token.'<br>';
+	echo 'openid: '.$openid.'<br>';
+
+	if($_GET['state'] === 'userinfo') {
+		$json = file_get_contents('https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN');
+		echo $json;
+		$json_object = json_decode($json);
+		$nickname = $json_object->nickname;
+		$sex = $json_object->sex;
+		$province = $json_object->province;
+		$city = $json_object->city;
+		$country = $json_object->country;
+		$headimgurl = $json_object->headimgurl;
+		echo 'nickname: '.$nickname.'<br>';
+		echo 'sex: '.$sex.'<br>';
+		echo 'province: '.$province.'<br>';
+		echo 'city: '.$city.'<br>';
+		echo 'country: '.$country.'<br>';
+		echo '<img src="'.$headimgurl.'"><br>';
+	}
+} else {
+	echo "woops, something goes wrong.";
+}
+?>
